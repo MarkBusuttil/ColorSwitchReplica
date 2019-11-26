cb564aee11e16bf7ceb8d5401f8daf28985fb6aa
Mark Busuttil <mark.busuttil.09@gmail.com>
2019-11-26 15:09:28 +0100

    Fixed ColorChanger/ FinishLine

---
+++ Colour Switch Replica/Assets/Script/Player.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum PlayerColor
+{
+	Cyan, Yellow, Purple, Pink
+}
+
+public class Player : MonoBehaviour
+{
+    public float jumpForce = 10;
+
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+
+    public PlayerColor currentColor;
+
+    public Color colorCyan;
+	public Color colorYellow;
+	public Color colorPurple;
+	public Color colorPink;
+
+	List<PlayerColor> colors = new List<PlayerColor>(){ PlayerColor.Cyan, PlayerColor.Yellow, PlayerColor.Pink, PlayerColor.Purple };
+
+    void Start ()
+	{
+		SetRandomColor();
+	}
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown (0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        
+    }
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.tag == "ColorChanger")
+		{
+			SetRandomColor();
+			Destroy(col.gameObject);
+			return;
+		}
+
+		if (col.tag != currentColor.ToString())
+		{
+			Debug.Log("GAME OVER!");
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+
+		if (col.tag == "FinishLine")
+		{
+			Debug.Log("NICENICENICENICE");
+		}
+	}
+
+		void SetRandomColor ()
+	{
+		int index = Random.Range(0, colors.Count);
+		PlayerColor newColor = colors[index];
+
+		colors.Remove(newColor);
+		if (!colors.Contains(currentColor)) colors.Add(currentColor);
+
+		currentColor = newColor;
+
+		switch (currentColor)
+		{
+			case PlayerColor.Cyan:
+				sr.color = colorCyan;
+				break;
+			case PlayerColor.Yellow:
+				sr.color = colorYellow;
+				break;
+			case PlayerColor.Purple:
+				sr.color = colorPurple;
+				break;
+			case PlayerColor.Pink:
+				sr.color = colorPink;
+				break;
+		}
+	}
+}
